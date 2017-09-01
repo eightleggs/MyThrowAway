abe7b2dac9c21e4a87ea136ac2c25efd7761c8cf
Andy Wang <me@test.com>
2017-08-31 20:10:05 -0700

    Initial commit

---
+++ LearningApplication/LearningApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LearningApplication.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LearningApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new HelloWorldViewModel();
+            model.ModelName = "My veryfirst Model";
+            model.HelloTimeStamp = DateTime.Now;
+            return View(model);
+        }
+
+        public IActionResult DestroyIt()
+        {
+            var model = new HelloWorldViewModel();
+            model.ModelName = "Destroyed";
+            model.HelloTimeStamp = DateTime.Now;
+            return View("index", model);
+
+        }
+    }
+}
---
+++ LearningApplication/LearningApplication/ViewModels/HelloWorldViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningApplication.ViewModels
+{
+    public class HelloWorldViewModel
+    {
+        public string ModelName { get; set; }
+        public DateTime HelloTimeStamp { get; set; }
+    }
+}
